79609d37eeb76495541ed5a237363c70487f9b45
= <=>
2020-10-19 21:36:32 +0700

    add-intro-slider

--- App.js
+++ App.js
@@ -1,47 +1,82 @@
-import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Routes from './src/Routes/Routes';
 
-// screen
-import WelcomeScreen from './src/pages/Welcome/WelcomeScreen';
-import LoginScreen from './src/pages/Login/LoginScreen';
-import RegisterScreen from './src/pages/Register/RegisterScreen';
-
-// custom hidden header of Stack
-const navOptionHandler = () => ({
-  headerShown: false,
-});
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Welcome"
-            component={WelcomeScreen}
-            options={navOptionHandler}
-          />
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-          />
-          <Stack.Screen
-            name="Register"
-            component={RegisterScreen}
-            options={navOptionHandler}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const slides = [
+  {
+    key: 'one',
+    title: 'JUST TRAVEL',
+    text:
+      'Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor',
+    image: require('./src/assets/images/workplace.png'),
   },
-});
+  {
+    key: 'two',
+    title: 'TAKE A BREAK',
+    text:
+      'Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor',
+    image: require('./src/assets/images/logo_login.png'),
+  },
+  {
+    key: 'three',
+    title: 'ENJOY YOUR JOURNEY',
+    text:
+      'Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor',
+    image: require('./src/assets/images/register.png'),
+  },
+];
+
+export default class App extends Component {
+  state = {showHomePage: false};
+  _renderItem = ({item}) => {
+    return (
+      <View style={{flex: 1}}>
+        <Image
+          source={item.image}
+          style={{
+            resizeMode: 'cover',
+            height: '73%',
+            width: '100%',
+          }}
+        />
+        <Text
+          style={{
+            paddingTop: 25,
+            paddingBottom: 10,
+            fontSize: 23,
+            fontWeight: 'bold',
+            color: '#21465b',
+            alignSelf: 'center',
+          }}>
+          {item.title}
+        </Text>
+
+        <Text
+          style={{
+            textAlign: 'center',
+            color: '#b5b5b5',
+            fontSize: 15,
+            paddingHorizontal: 30,
+          }}>
+          {item.text}
+        </Text>
+      </View>
+    );
+  };
+  render() {
+    if (this.state.showHomePage) {
+      return <Routes />;
+    } else
+      return (
+        <AppIntroSlider
+          renderItem={this._renderItem}
+          data={slides}
+          activeDotStyle={{
+            backgroundColor: '#21465b',
+            width: 30,
+          }}
+        />
+      );
+  }
+}
